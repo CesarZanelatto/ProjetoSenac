9eb0e793427851e4e700b34d233d35cbc73308a0
csrッ <99185429+CesarZanelatto@users.noreply.github.com>
2026-05-12 13:06:01 -0300

    refactor(paciente.py e agendamento.py) adiciona getters e setters
    Getters e Setters adicionados as entitys paciente e agendamento

--- src/models/entitys/paciente.py
+++ src/models/entitys/paciente.py
@@ -2,10 +2,34 @@
 class Paciente:
 
     def __init__(self,id:int,nome:str,data_nascimento:str,telefone:str):
-        self.id=id
-        self.nome=nome
-        self.data_nascimento=data_nascimento
-        self.telefone=telefone
+        self.__id=id
+        self.__nome=nome
+        self.__data_nascimento=data_nascimento
+        self.__telefone=telefone
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self,nome):
+        self.__nome=nome
+
+    @property
+    def data_nascimento(self):
+        return self.__data_nascimento
+
+    @property
+    def telefone(self):
+        return self.__telefone
+
+    @telefone.setter
+    def telefone(self,telefone):
+        self.__telefone=telefone
 
     def to_dict(self):
         return {
